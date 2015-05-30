a4bb58fb32963095fbf29d364d08bce4c351c19f
Michał Jendrzejewski <michaljendrzejewski@MOB-NOT71.local>
2015-05-30 20:20:07 +0200

    resizing added

--- src/js/planting/planting-handlers.js
+++ src/js/planting/planting-handlers.js
@@ -1,224 +1,238 @@
 Planting.prototype.toggle_start_button = function () {
-    var that = this;
-    return function () {
-        if (that.pano.getVisible()) {
-            that.startBtn.show();
-        } else {
-            that.startBtn.hide();
-        }
-    };
+  var that = this;
+  return function () {
+    if (that.pano.getVisible()) {
+      that.startBtn.show();
+    } else {
+      that.startBtn.hide();
+    }
+  };
 };
 
 Planting.prototype.start_planting = function () {
-    var that = this;
-    return function () {
-        that.overlay.show();
-        that.toolbox.show();
-        that.startBtn.hide();
-        that.pano.setOptions({
-            panControl: false,
-            zoomControl: false,
-            addressControl: false,
-            linksControl: false,
-        });
+  var that = this;
+  return function () {
+    that.overlay.show();
+    that.toolbox.show();
+    that.startBtn.hide();
+    that.pano.setOptions({
+      panControl: false,
+      zoomControl: false,
+      addressControl: false,
+      linksControl: false,
+    });
 
-    };
+  };
 };
 
 Planting.prototype.plant_object = function () {
-    var that = this;
-    return function (e, ui) {
-        for (var i = 0; i < that.toolboxobjects.length; i++) {
-            if (ui.draggable.is(that.toolboxobjects[i].draggable)) {
+  var that = this;
+  return function (e, ui) {
+    for (var i = 0; i < that.toolboxobjects.length; i++) {
+      if (ui.draggable.is(that.toolboxobjects[i].draggable)) {
 
-                var containerOffset = that.container.offset();
-                var objectOffset = that.toolboxobjects[i].draggable.offset();
-                var top = objectOffset.top - containerOffset.top;
-                var left = objectOffset.left - containerOffset.left;
+        var containerOffset = that.container.offset();
+        var objectOffset = that.toolboxobjects[i].draggable.offset();
+        var top = objectOffset.top - containerOffset.top;
+        var left = objectOffset.left - containerOffset.left;
 
-                var img = $('<img />').attr('src', that.toolboxobjects[i].projections[0]);
-                var uiTrash = $('<span class="icon-trash" />');
-                var uiResize = $('<span class="icon-resize" />');
-                var uiRotate = $('<span class="icon-loop" />');
-                var uiRotateWrapper = $('<div class="wrapper-rotate" />')
-                    .append('<span class="icon-menu-left" />')
-                    .append('<span class="degrees">0&deg;</span>')
-                    .append('<span class="icon-menu-right" />')
-                    .append(uiRotate);
-                var tools = $('<div class="plantingjs-plantedobject-tools" />')
-                    .append(uiTrash)
-                    .append(uiResize)
-                    .append(uiRotateWrapper);
+        var img = $('<img />').attr('src', that.toolboxobjects[i].projections[0]);
+        var uiTrash = $('<span class="icon-trash" />');
+        var uiResize = $('<span class="icon-resize" />');
+        var uiRotate = $('<span class="icon-loop" />');
+        var uiRotateWrapper = $('<div class="wrapper-rotate" />')
+          .append('<span class="icon-menu-left" />')
+          .append('<span class="degrees">0&deg;</span>')
+          .append('<span class="icon-menu-right" />')
+          .append(uiRotate);
+        var tools = $('<div class="plantingjs-plantedobject-tools" />')
+          .append(uiTrash)
+          .append(uiResize)
+          .append(uiRotateWrapper);
 
-                var container = $('<div class="plantingjs-plantedobject-container">')
-                    .offset({ top: ui.offset.top, left: ui.offset.left})
-                    .draggable({ cancel: ".icon-loop, .icon-trash, .icon-resize"})
-                    .append(img)
-                    .append(tools);
+        var container = $('<div class="plantingjs-plantedobject-container">')
+          .offset({ top: ui.offset.top, left: ui.offset.left})
+          .draggable({ cancel: ".icon-loop, .icon-trash, .icon-resize"})
+          .append(img)
+          .append(tools);
 
-                that.overlay.append(container);
+        that.overlay.append(container);
 
-                var plant = {
-                    object: i,
-                    projection: 0,
-                    container: container,
-                    tools: tools,
-                    img: img,
-                };
+        var plant = {
+          object: i,
+          projection: 0,
+          container: container,
+          tools: tools,
+          img: img,
+        };
 
-                uiResize.on('mousedown', {plantedObject: plant}, that.resize_object);
-                uiRotate.on('mousedown', {toolBoxObjects: that.toolboxobjects, plantedObject: plant}, that.rotate_object);
-                uiTrash.on('click', that.remove_object);
-                that.plantedobjects.push(plant);
-                that.toolboxobjects[i].draggable.css({'top': '0px', 'left': '0px'});
-            }
-        }
-    };
+        uiResize.on('mousedown', {plantedObject: plant}, that.resize_object);
+        uiRotate.on('mousedown', {toolBoxObjects: that.toolboxobjects, plantedObject: plant}, that.rotate_object);
+        uiTrash.on('click', that.remove_object);
+        that.plantedobjects.push(plant);
+        that.toolboxobjects[i].draggable.css({'top': '0px', 'left': '0px'});
+      }
+    }
+  };
 };
 
 Planting.prototype.plant_objects_for_view = function () {
-    var that = this;
-    return function (data) {
-        for (var i = 0; i < that.plantedobjects.length; i++) {
-            that.plantedobjects[i].url = data.toolboxobjects[that.plantedobjects[i].object].projections[that.plantedobjects[i].projection];
-            var img = $('<img />')
-                .attr('src', that.plantedobjects[i].url);
-            var containerOffset = that.container.offset();
-            var div = $('<div class="plantingjs-plantedobject-container" />')
-                .offset({
-                    left: containerOffset.left + that.plantedobjects[i].position.x * that.width,
-                    top: containerOffset.top + that.height / 2 + that.plantedobjects[i].position.y * that.width
-                });
-            div.append(img);
-            that.overlay.append(div);
-            that.plantedobjects[i].img = img;
-            that.plantedobjects[i].container = div;
-        }
-    };
+  var that = this;
+  return function (data) {
+    for (var i = 0; i < that.plantedobjects.length; i++) {
+      that.plantedobjects[i].url = data.toolboxobjects[that.plantedobjects[i].object].projections[that.plantedobjects[i].projection];
+      var img = $('<img />')
+        .attr('src', that.plantedobjects[i].url);
+      var containerOffset = that.container.offset();
+      var div = $('<div class="plantingjs-plantedobject-container" />')
+        .offset({
+          left: containerOffset.left + that.plantedobjects[i].position.x * that.width,
+          top: containerOffset.top + that.height / 2 + that.plantedobjects[i].position.y * that.width
+        });
+      div.append(img);
+      that.overlay.append(div);
+      that.plantedobjects[i].img = img;
+      that.plantedobjects[i].container = div;
+    }
+  };
 };
 
 function calc_direction(objectPosition, mousePosition) {
-    var calc = {};
-    if (objectPosition <= mousePosition) {
-        calc = {
-            direction: true,
-            mouseObjectDiff: mousePosition - objectPosition
-        };
-    } else if (objectPosition > mousePosition) {
-        calc = {
-            direction: false,
-            mouseObjectDiff: objectPosition - mousePosition
-        };
-    }
-    return calc;
+  var calc = {};
+  if (objectPosition <= mousePosition) {
+    calc = {
+      direction: true,
+      mouseObjectDiff: mousePosition - objectPosition
+    };
+  } else if (objectPosition > mousePosition) {
+    calc = {
+      direction: false,
+      mouseObjectDiff: objectPosition - mousePosition
+    };
+  }
+  return calc;
 }
 
 function calc_sides(mouseObjectDiff, projectionsLength) {
-    var sideLength = 20;
-    var sides = (mouseObjectDiff - (mouseObjectDiff % sideLength)) / sideLength;
-    if (sides >= projectionsLength) {
-        sides = sides % projectionsLength;
-    }
-    return sides;
+  var sideLength = 20;
+  var sides = (mouseObjectDiff - (mouseObjectDiff % sideLength)) / sideLength;
+  if (sides >= projectionsLength) {
+    sides = sides % projectionsLength;
+  }
+  return sides;
 }
 
 var rotateHelpers = {
-    calc_direction: calc_direction,
-    calc_sides: calc_sides
+  calc_direction: calc_direction,
+  calc_sides: calc_sides
 };
 
 
 var EVENT_MOUSEDOWN = false;
 Planting.prototype.rotate_object = function (e) {
-    EVENT_MOUSEDOWN = true;
-    $('body').addClass('noselect rotate');
+  EVENT_MOUSEDOWN = true;
+  $('body').addClass('noselect rotate');
 
-    var plantedObject = e.data.plantedObject;
-    var objectProjections = e.data.toolBoxObjects[plantedObject.object].projections;
+  var plantedObject = e.data.plantedObject;
+  var objectProjections = e.data.toolBoxObjects[plantedObject.object].projections;
 
-    plantedObject.container.addClass('plantingjs-active-object');
+  plantedObject.container.addClass('plantingjs-active-object');
 
-    var projection;
-    var buttonX = $(this).offset().left;
+  var projection;
+  var buttonX = $(this).offset().left;
 
-    function calc_projection(objectPosition, mousePosition) {
-        var newProjection;
-        var currentSide = plantedObject.projection;
-        var direction = rotateHelpers.calc_direction(objectPosition, mousePosition);
-        var numberOfSides = rotateHelpers.calc_sides(direction.mouseObjectDiff, objectProjections.length);
+  function calc_projection(objectPosition, mousePosition) {
+    var newProjection;
+    var currentSide = plantedObject.projection;
+    var direction = rotateHelpers.calc_direction(objectPosition, mousePosition);
+    var numberOfSides = rotateHelpers.calc_sides(direction.mouseObjectDiff, objectProjections.length);
 
-        currentSide++;
-        if (direction.direction) {
-            if ((currentSide + numberOfSides) > objectProjections.length) {
-                newProjection = (currentSide + numberOfSides) % objectProjections.length;
-            } else {
-                newProjection = currentSide + numberOfSides;
-            }
-        } else {
-            if (currentSide <= numberOfSides) {
-                currentSide += objectProjections.length;
-            }
-            newProjection = currentSide - numberOfSides;
-        }
-        return --newProjection;
+    currentSide++;
+    if (direction.direction) {
+      if ((currentSide + numberOfSides) > objectProjections.length) {
+        newProjection = (currentSide + numberOfSides) % objectProjections.length;
+      } else {
+        newProjection = currentSide + numberOfSides;
+      }
+    } else {
+      if (currentSide <= numberOfSides) {
+        currentSide += objectProjections.length;
+      }
+      newProjection = currentSide - numberOfSides;
     }
+    return --newProjection;
+  }
 
-    function rotateOnMove(e) {
-        if (!EVENT_MOUSEDOWN) return;
-        if (plantedObject.container.hasClass('plantingjs-active-object')) {
-            projection = calc_projection(buttonX, e.pageX);
-            plantedObject.img = plantedObject.img.attr('src', objectProjections[projection]);
-        }
+  function rotateOnMove(e) {
+    if (!EVENT_MOUSEDOWN) return;
+    if (plantedObject.container.hasClass('plantingjs-active-object')) {
+      projection = calc_projection(buttonX, e.pageX);
+      plantedObject.img = plantedObject.img.attr('src', objectProjections[projection]);
     }
-    $(document).mousemove(rotateOnMove);
+  }
 
-    $(document).mouseup(function(e) {
-        var plantedObjectContainer = plantedObject.container;
-        if (EVENT_MOUSEDOWN && plantedObjectContainer.hasClass('plantingjs-active-object')) {
-            plantedObject.projection = projection;
-            $('body').removeClass('noselect rotate');
-            plantedObjectContainer.removeClass('plantingjs-active-object');
-            EVENT_MOUSEDOWN = false;
-        }
-    });
+  function finishRotation (e) {
+    var plantedObjectContainer = plantedObject.container;
+    if (EVENT_MOUSEDOWN && plantedObjectContainer.hasClass('plantingjs-active-object')) { console.log(54453);
+      $(document).off('mousemove', rotateOnMove);
+      $(document).off('mouseup', finishRotation);
+      plantedObject.projection = projection;
+      $('body').removeClass('noselect rotate');
+      plantedObjectContainer.removeClass('plantingjs-active-object');
+      EVENT_MOUSEDOWN = false;
+    }
+  }
+  $(document).on('mousemove', rotateOnMove);
+
+  $(document).on('mouseup',finishRotation);
 };
 
-// Planting.prototype.resize_object = function (e) {
-//     EVENT_MOUSEDOWN = true;
-//     $('body').addClass('noselect resize');
-//     var plantedObject = e.data.plantedObject;
-//     plantedObject.container.addClass('plantingjs-active-object');
-//
-//     buttonX = $(this).offset().left;
-//
-//     var resizeOnDrag = function(e) {
-//         if (!EVENT_MOUSEDOWN) return;
-//         if (plantedObject.container.hasClass('plantingjs-active-object')) {
-//             buttonCursorDistance = buttonX - e.pageX;
-//             if (buttonCursorDistance === 0) {
-//                 scale = 1;
-//             } else if (buttonCursorDistance < 0) {
-//                 scale = 1 - Math.abs(buttonCursorDistance/100);
-//             } else if (buttonCursorDistance > 0) {
-//                 scale = 1 + buttonCursorDistance/100;
-//             }
-//             plantedObject.img = plantedObject.img.css('transform', 'scale(' + scale + ')');
-//         }
-//     };
-//
-//     $(document).on('mousemove', resizeOnDrag);
-//
-//     $(document).mouseup(function(e) {
-//         var plantedObjectContainer = plantedObject.container;
-//         if (EVENT_MOUSEDOWN && plantedObjectContainer.hasClass('plantingjs-active-object')) {
-//             plantedObject.scale = scale;
-//             $('body').removeClass('noselect resize');
-//             plantedObjectContainer.removeClass('plantingjs-active-object');
-//             EVENT_MOUSEDOWN = false;
-//         }
-//     });
-// };
+Planting.prototype.resize_object = function (e) {
+  EVENT_MOUSEDOWN = true;
+  $('body').addClass('noselect resize');
+  var plantedObject = e.data.plantedObject,
+    scale = plantedObject.scale || 1,
+    cssscale;
+  plantedObject.container.addClass('plantingjs-active-object');
+  console.log(plantedObject.scale);
+
+  buttonX = $(this).offset().left;
+
+  var resizeOnDrag = function(e) {
+      if (!EVENT_MOUSEDOWN) return;
+      if (plantedObject.container.hasClass('plantingjs-active-object')) {
+        var buttonCursorDistance = buttonX - e.pageX;
+        //console.log(buttonCursorDistance,  3333333)
+        if (buttonCursorDistance === 0) {
+          cssscale = scale;
+        } else if (buttonCursorDistance < 0) {
+          cssscale = scale - Math.abs(buttonCursorDistance/100);
+        } else if (buttonCursorDistance > 0) {
+          cssscale = scale + buttonCursorDistance/100;
+        }
+
+        if (cssscale > 0.2)
+          plantedObject.img = plantedObject.img.css('transform', 'scale(' + cssscale + ')');
+      }
+    },
+    finishResizing = function (e) {
+      var plantedObjectContainer = plantedObject.container;
+      if (EVENT_MOUSEDOWN && plantedObjectContainer.hasClass('plantingjs-active-object')) {
+        plantedObject.scale = cssscale; console.log(plantedObject.scale)
+        $('body').removeClass('noselect resize');
+        plantedObjectContainer.removeClass('plantingjs-active-object');
+        $(document).off('mousemove', resizeOnDrag);
+        $(document).off('mouseup', finishResizing);
+        EVENT_MOUSEDOWN = false;
+      }
+    }
+
+
+  $(document).on('mousemove', resizeOnDrag);
+
+  $(document).on('mouseup', finishResizing);
+};
 
 Planting.prototype.remove_object = function (e) {
-    $(this).closest('.plantingjs-plantedobject-container').remove();
+  $(this).closest('.plantingjs-plantedobject-container').remove();
 };
