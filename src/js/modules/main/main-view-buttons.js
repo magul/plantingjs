7990a05f42073cfff6fe46cd5711cf4c9d06c5cb
cmwd <pwojtkowiak@gmail.com>
2016-05-24 00:06:56 +0200

    Refactor main.js module
    My goal was to simplify `initialize` method and introduce DRY way of creating big button.
    I created private method for handling events. I think now it's more clear what this module does.

---
+++ src/js/modules/main/main-view-buttons.js
@@ -0,0 +1,25 @@
+import Button from '../components/button';
+
+function createButton(label, eventHash, eventContext) {
+  const instance = new Button({
+    modifier: 'accept',
+    visible: false,
+    label });
+
+  for (const event in eventHash) {
+    if (!eventHash[event].hasOwnProperty()) {
+      instance.on(event, eventHash[event], eventContext);
+    }
+  }
+
+  return instance;
+}
+
+export const submitButton = (...args) =>
+  createButton('zrobione!', ...args);
+
+export const initButton = (...args) =>
+  createButton('zacznij wysadzać!', ...args);
+
+export const selectButton = (...args) =>
+  createButton('wybierz', ...args);
