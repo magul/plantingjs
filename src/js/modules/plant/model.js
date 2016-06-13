41fecea4ebbafb5219f9042baaa0e90609c9c525
cmwd <pwojtkowiak@gmail.com>
2016-06-13 21:52:37 +0200

    Introduction to moveable api (resolves #214)

--- src/js/modules/plant/model.js
+++ src/js/modules/plant/model.js
@@ -12,17 +12,34 @@ export default Model.extend({
     userActivity: false,
   },
 
-  getProjection: function() {
-    return this.manifesto()
-      .getProjectionsFor(this.get('objectId'))[
-        this.get('currentProjection') || this.get('projection') || 0
-      ];
+  getProjection() {
+    return this.get('projections')[this.get('projection')];
   },
 
-  setProjection: function(at_) {
+  setProjection(at_) {
     const projections = this.get('projections');
     let at = at_ > projections.length ? 0 : at_;
     at = at < 0 ? projections.length : at;
     this.set('projection', at);
+  },
+
+  setPosX({ x, width }) {
+    this.set('x', x / width);
+
+    return this;
+  },
+
+  getPosX({ width }) {
+    return width * this.get('x');
+  },
+
+  setPosY({ y, height, width }) {
+    this.set('y', (y - (height / 2)) / width );
+
+    return this;
+  },
+
+  getPosY({ width, height }) {
+    return height / 2 + this.get('y') * width;
   },
 });
